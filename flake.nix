d11a5678fdeea1ba387039f5c68809cffdb99fd0
Hex <me@hexade.ca>
2025-07-06 17:08:46 +0300

    Rebase to 2.29.1

--- flake.nix
+++ flake.nix
@@ -11,41 +11,41 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: rec {
-      checks = {
-        inherit (packages)
-          # nix-dram-progress # broken
-          nix-dram
-          nix-search nix-search-pretty
-          nix-nar-listing;
-      };
-
-      defaultPackage = packages.nix-dram;
-
-      defaultApp = apps.nix-dram;
-
-      apps = {
-        nix-dram = {
-          type = "app";
-          program = "${packages.nix-dram}/bin/nix";
-        };
-
-        default = apps.nix-dram;
-      };
-
-      packages =
-        let pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay ];
-        };
-        in {
-          inherit (pkgs)
-            nix-dram nix-dram-progress
+        checks = {
+          inherit (packages)
+            # nix-dram-progress # broken
+            nix-dram
             nix-search nix-search-pretty
             nix-nar-listing;
-
-          default = packages.nix-dram;
         };
-    }) // {
+
+        defaultPackage = packages.nix-dram;
+
+        defaultApp = apps.nix-dram;
+
+        apps = {
+          nix-dram = {
+            type = "app";
+            program = "${packages.nix-dram}/bin/nix";
+          };
+
+          default = apps.nix-dram;
+        };
+
+        packages =
+        let pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlay ];
+            };
+        in {
+            inherit (pkgs)
+              nix-dram nix-dram-progress
+              nix-search nix-search-pretty
+              nix-nar-listing;
+
+            default = packages.nix-dram;
+          };
+      }) // {
       overlay = final: prev: {
         nix-search-pretty =
           final.haskellPackages.callPackage ./nix-search-pretty {};
@@ -54,13 +54,12 @@
           final.haskellPackages.callPackage ./nix-nar-listing {};
 
         make-nix-dram = { nix }:
-          nix.overrideAttrs (old: {
+          (nix.appendPatches [
+            ./nix-patches/nix-flake-default.patch
+            ./nix-patches/nix-search-meta.patch
+            ./nix-patches/nix-environment.patch
+          ]).overrideAttrs (old: {
             name = "nix-dram-" + old.version;
-            patches = [
-              ./nix-patches/nix-flake-default.patch
-              ./nix-patches/nix-search-meta.patch
-              ./nix-patches/nix-environment.patch
-            ];
           });
 
         nix-dram = final.make-nix-dram {
@@ -68,20 +67,20 @@
         };
 
         nix-dram-progress = final.callPackage ({ nix }: nix.overrideAttrs (old: {
-          name = "nix-dram-" + old.version;
+            name = "nix-dram-" + old.version;
           patches = (old.patches or []) ++ [
-            ./nix-patches/nix-flake-default.patch
-            ./nix-patches/nix-search-meta.patch
-            ./nix-patches/nix-environment.patch
-            (final.fetchpatch {
-              name = "nix-progress.diff";
-              url = "https://github.com/NixOS/nix/compare/480426a...1af0a16.diff";
-              sha256 = "sha256-vR7kGQMLHcf2qnaycyrv8h9M5iZjIC+GxD9kfqM3lzQ=";
-            })
-          ];
+              ./nix-patches/nix-flake-default.patch
+              ./nix-patches/nix-search-meta.patch
+              ./nix-patches/nix-environment.patch
+              (final.fetchpatch {
+                name = "nix-progress.diff";
+                url = "https://github.com/NixOS/nix/compare/480426a...1af0a16.diff";
+                sha256 = "sha256-vR7kGQMLHcf2qnaycyrv8h9M5iZjIC+GxD9kfqM3lzQ=";
+              })
+            ];
         })) {
-          nix = final.nixVersions.latest;
-        };
+            nix = final.nixVersions.latest;
+          };
 
         nix-search = final.writeShellScriptBin "nix-search" ''
           ${final.nix-dram}/bin/nix search --json "$@" | ${final.nix-search-pretty}/bin/nix-search-pretty
